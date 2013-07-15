1aa6f9d0addda7f82134699db04b7a3a68947477
Sherljohn <734186722@qq.com>
2013-07-15 15:45:46 +0800

    add advanced cart

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '256e8614fbc11b15500612d17d32b2b9b0f63b74fd8d26c178942b2720c8cf60222920e9d0340df4c3c548ad65c6703855b6789409e5f22d5dfd242ffbe286e1'
