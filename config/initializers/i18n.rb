285af8c1d43cd9878840d973fe15fa4a4739aad5
BinQi <1264135980@qq.com>
2013-07-19 10:57:31 +0800

    Add Japanese version

--- config/initializers/i18n.rb
+++ config/initializers/i18n.rb
@@ -4,5 +4,6 @@ I18n.default_locale = :en
 LANGUAGES = [
   ['English',                  'en'],
   ["Espa&ntilde;ol".html_safe, 'es'],
-  ["中文",                     'ch']   
+  ["中文",                     'ch'],
+  ["日本語",                   'jp']     
 ]
