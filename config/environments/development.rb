f1de6765ff0ef86ee63db34cb6cc54b7bc65d003
BinQi <1264135980@qq.com>
2013-07-26 13:25:09 +0800

    email heroku

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,17 @@
 Depot::Application.configure do
+  Depot::Application.configure do
+    config.action_mailer.delivery_method = :smtp
+    
+    config.action_mailer.smtp_settings = {
+      address:                "smtp.gmail.com",
+      port:                   587,
+      domain:                 "domain.of.sender.net",
+      authentication:         "plain",
+      user_name:              "imwubinqi@gmail.com",
+      password:               "19920529",
+      enable_starttls_auto:   true
+    }
+  end
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
