6f88473d8a39550d2832f7f7c2b1d8885266ff24
BinQi <1264135980@qq.com>
2013-07-26 17:04:20 +0800

    email heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,10 +64,7 @@ Depot::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  
-=begin
-  config.assets.version = '1.0'
-  config.action_mailer.default_url_options = { :host => 'desolate-cove-8767.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'myapp.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
@@ -75,11 +72,10 @@ Depot::Application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "desolate-cove-8767.herokuapp.com",
+    domain: "myapp.herokuapp.com",
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name:  "imwubinqi@gmail.com",
-    password: "w1992529"
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
   } 
-=end  
 end
