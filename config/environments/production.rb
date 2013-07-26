e0b31b7bd33e6e00714252022985ca577211f390
BinQi <1264135980@qq.com>
2013-07-26 16:49:14 +0800

    email heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,8 @@ Depot::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  
+=begin
   config.assets.version = '1.0'
   config.action_mailer.default_url_options = { :host => 'desolate-cove-8767.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
@@ -79,4 +81,5 @@ Depot::Application.configure do
     user_name:  "imwubinqi@gmail.com",
     password: "w1992529"
   } 
+=end  
 end
