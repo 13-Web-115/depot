bf5007c2bcc8fd5feb143395509c0715e3c2afc7
BinQi <1264135980@qq.com>
2013-07-26 15:09:20 +0800

    email heroku

--- config/application.rb
+++ config/application.rb
@@ -58,7 +58,7 @@ module Depot
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-    config.action_mailer.default_url_options = { :host => 'myapp.herokuapp.com' }
+    config.action_mailer.default_url_options = { :host => 'desolate-cove-8767.herokuapp.com' }
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.perform_deliveries = true
     config.action_mailer.raise_delivery_errors = false
@@ -66,7 +66,7 @@ module Depot
     config.action_mailer.smtp_settings = {
       address: "smtp.gmail.com",
       port: 587,
-      domain: "myapp.herokuapp.com",
+      domain: "desolate-cove-8767.herokuapp.com",
       authentication: "plain",
       enable_starttls_auto: true,
       user_name:  "imwubinqi@gmail.com",
