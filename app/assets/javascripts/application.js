518eed1fc3c30b9bbee387ba11f3b94befdac76e
BinQi <1264135980@qq.com>
2013-07-29 11:58:03 +0800

    Improve books category

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,20 @@
 //= require jquery-ui
 //= require jquery_ujs
 //= require_tree .
+function menus(str){ 
+	var obj; 
+	// 返回 str 对象和 picId 对象是否存在 
+	if (document.getElementById(str)){ 
+		obj=document.getElementById(str); //obj 为 DIV 对象 
+		//pic=document.getElementById(picId); //pic 为 图片对象 
+		
+		if (obj.style.display == "none"){ //如果DIV对象的 display 样式值为 none 的话 
+			obj.style.display = ""; //就将 display 的样式清空 
+			//pic.src = "0001.jpg"; //更改图片对象的路径 
+		}
+		else{ 
+			obj.style.display = "none"; 
+			//pic.src = "0000.jpg"; 
+		} 
+	} 
+} 
