4c7fbc58eb74589f91e36917482ce83fa4720345
BinQi <1264135980@qq.com>
2013-07-30 11:49:58 +0800

    fix one bug on products

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :password_digest, :password, :password_confirmation, :genre
   validates :name, presence:true, uniqueness:true
-  USER_TYPES = ["ordinary", "shopkeeper", "admin"]
+  USER_TYPES = ["ordinary", "shopkeeper"]#, "admin"]
   has_secure_password
   
   after_destroy :ensure_an_admin_remains
