f87b7e118f3ca922ee689638cf773da0b55476ba
a317216007 <a317216007@163.com>
2013-07-31 15:19:24 +0800

    update international v2.0

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Depot::Application.initialize!
+
+#WillPaginate::ViewHelpers.pagination_options[:previous_label] = "上"
+#WillPaginate::ViewHelpers.pagination_options[:next_label] = "下"
